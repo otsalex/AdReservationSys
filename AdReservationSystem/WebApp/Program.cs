8f079b45d3d93d0520f18e7409651f5a04c0be67
aleots <aleots@ttu.ee>
2023-03-15 19:25:56 +0100

    added JS 1. HW, with min requirements met

--- AdReservationSystem/WebApp/Program.cs
+++ AdReservationSystem/WebApp/Program.cs
@@ -1,4 +1,5 @@
 using DAL;
+using DAL.Seeding;
 using Domain;
 using Domain.App.Identity;
 using Microsoft.AspNetCore.Identity;
@@ -25,7 +26,12 @@ builder.Services.AddIdentity<AppUser, AppRole>(options => options.SignIn.Require
 
 builder.Services.AddControllersWithViews();
 
+
 var app = builder.Build();
+
+// setup database stuff
+SetupAppData(app, app.Environment, app.Configuration);
+
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
@@ -51,3 +57,60 @@ app.MapControllerRoute(
 app.MapRazorPages();
 
 app.Run();
+
+static void SetupAppData(IApplicationBuilder app, IWebHostEnvironment environment, IConfiguration configuration)
+{
+    
+    using var serviceScope = app.ApplicationServices
+        .GetRequiredService<IServiceScopeFactory>()
+        .CreateScope();
+    using var context = serviceScope.ServiceProvider.GetService<ApplicationDbContext>();
+    
+    if(context == null)
+    {
+        throw new ApplicationException("Problem in services. Can't initialize DB context.");
+    }
+
+    using var UserManager = serviceScope.ServiceProvider.GetService<UserManager<AppUser>>();
+    using var RoleManager = serviceScope.ServiceProvider.GetService<RoleManager<AppRole>>();
+    if (UserManager == null || RoleManager == null)
+    {
+    }
+
+    var logger = serviceScope.ServiceProvider.GetService<ILogger<IApplicationBuilder>>();
+    if (logger == null)
+    {
+        throw new ApplicationException("Problem in services. Can't initialize logger.");
+    }
+
+    if (context.Database.ProviderName!.Contains("InMemory"))
+    {
+        return;
+    }
+    // TODO: Wait for db connection
+    
+    // drop
+    if (configuration.GetValue<bool>("DataInit:DropDatabase"))
+    {
+        logger.LogWarning("Dropping database");
+        AppDataInit.DropDatabase(context);
+    }
+    // migrate
+    if (configuration.GetValue<bool>("DataInit:MigrateDatabase"))
+    {
+        logger.LogInformation("Migrating database");
+        AppDataInit.MigrateDatabase(context);
+    }
+    // seed identity
+    if (configuration.GetValue<bool>("DataInit:SeedIdentity"))
+    {
+        logger.LogInformation("Seeding identity");
+        AppDataInit.SeedIdentity();
+    }
+    // seed application data
+    if (configuration.GetValue<bool>("DataInit:SeedData"))
+    {
+        logger.LogWarning("Seed app data");
+        AppDataInit.SeedAppData(context);
+    }
+}
